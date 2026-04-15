b4b5e2ab94d11e4aed461953ce1d62f5d4b95448
Tianyu Wu <tianyu2206@gmail.com>
2026-04-15 19:36:29 -0400

    added unet model

---
+++ src/main.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn, torch.optim as optim
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+from trainer import Trainer
+from dataset import getDataset
+from model import UNet
+import constants as const
+
+savePath = "trainedModel/UNet1.pth"
+lr = 1e-4
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+trainDataset, valDataset, testDataset = getDataset(const.CELLNUCLEI_FILE_PATH)
+
+model = UNet(const.CELLNUCLEI_IN_CHANNELS, const.CELLNUCLEI_OUT_CHANNELS, 32, 4)
+
+trainLoader = DataLoader(dataset=trainDataset, batch_size=4, shuffle=True)
+valLoader = DataLoader(dataset=valDataset, batch_size=4, shuffle=True)
+
+optimizer = optim.AdamW(model.parameters(), lr=lr)
+criterion = nn.BCEWithLogitsLoss()
+
+trainer = Trainer(model, trainLoader, valLoader, optimizer, criterion, device)
+trainer.train(50, patience=5, saveModel=True, saveModelPath=savePath)
+
+
+# model = model.to(device)
+# model.load_state_dict(torch.load(savePath, map_location=device))
+
+# model.eval()
+
+# for i in range (5):
+#     img, mask = testDataset[i]
+
+#     with torch.no_grad():
+#         img_tensor = img.unsqueeze(0).to(device)
+
+#         # UNet prediction
+#         pred_unet = model(img_tensor)
+#         pred_unet = torch.sigmoid(pred_unet)
+#         mask_unet = (pred_unet > 0.5).float().squeeze().cpu().numpy()
+
+#     plt.figure(figsize=(15, 5))
+
+#     # Original image
+#     plt.subplot(1, 4, 1)
+#     plt.imshow(img.permute(1, 2, 0))
+#     plt.title("Original Image")
+#     plt.axis("off")
+
+#     # UNet prediction
+#     plt.subplot(1, 4, 2)
+#     plt.imshow(mask_unet, cmap="gray")
+#     plt.title("UNet Prediction")
+#     plt.axis("off")
+
+#     # Ground truth
+#     plt.subplot(1, 4, 3)
+#     plt.imshow(mask.squeeze(), cmap="gray")
+#     plt.title("Ground Truth")
+#     plt.axis("off")
+
+#     plt.tight_layout()
+#     plt.show()
---
+++ src/model.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+def DoubleConv(inChannels, outChannels, p=0.1):
+    return nn.Sequential(
+        nn.Conv2d(inChannels, outChannels, 3, padding="same", bias=False),
+        nn.BatchNorm2d(outChannels),
+        nn.ReLU(inplace=False),
+
+        nn.Conv2d(outChannels, outChannels, 3, padding="same", bias=False),
+        nn.BatchNorm2d(outChannels),
+        nn.ReLU(inplace=False),
+        nn.Dropout2d(p),
+    )
+
+class UNet(nn.Module):
+    def __init__(self, imgChannels: int, outChannels: int, initFeatures: int, numLayers: int):
+        super(UNet, self).__init__()
+
+        self.downLayers = nn.ModuleList()
+        self.upLayers = nn.ModuleList()
+        self.features = [initFeatures * (2**(i)) for i in range(numLayers - 1)]
+
+        for numF in self.features:
+            self.downLayers.append(DoubleConv(imgChannels, numF))
+            self.downLayers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+            imgChannels = numF
+        
+        for numF in reversed(self.features):
+            self.upLayers.append(nn.ConvTranspose2d(numF*2, numF, kernel_size=2, stride=2))
+            self.upLayers.append(DoubleConv(numF*2, numF))
+        
+        self.bottleneckLayer = DoubleConv(self.features[-1], self.features[-1]*2)
+        self.finalConv = nn.Conv2d(self.features[0], outChannels, kernel_size=1)
+
+    def forward(self, x):
+        skipConnections = []
+
+        for i in range(0, len(self.downLayers), 2):
+            x = self.downLayers[i](x)
+            skipConnections.append(x)
+            x = self.downLayers[i+1](x)
+        
+        x = self.bottleneckLayer(x)
+        skipConnections.reverse()
+        
+        for i in range(0, len(self.downLayers), 2):
+            x = self.upLayers[i](x)
+            sc = skipConnections[i//2]
+            addedSC = torch.cat((sc, x), dim=1)
+            x = self.upLayers[i+1](addedSC)
+        
+        return self.finalConv(x)
