91dfcc981dac4bbe09c0a3951e87911f856c3f77
Ernest-Barr <embarrington3@gmail.com>
2026-04-24 14:42:58 -0400

    Updated readme.md and requirements

--- readme.md
+++ readme.md
@@ -1,1 +1,69 @@
-# Unet for Point Cloud Segmentation 
+# Analysis of UNet Architectures for Medical Image Segmentation 
+
+This repository provides a standardized benchmark for analyzing the performance of various Unet architectures against dataests from MedSegBench. 
+
+## Requirements & Setup
+
+### Environment Setup 
+ 
+```bash
+conda create -n medmnist-attack python=3.10.9
+conda activate medmnist-attack
+pip install -r requirements 
+```
+
+### Running Training/Testing
+
+#### Datasets
+
+Unfortunately, the automatic download of the datasets through the MedSegBench python package does not seem to work. To alleviate this please download the data sets from the [official zenodo page](https://zenodo.org/records/13358372) and paste the desired datasets that are selected in [config.py](src/config.py) in the data/ directory of the root folder.
+
+#### Setting up the Config
+The following lines in [config.py](src/config.py) can be configured to change the datasets/models used in the experiment. 
+All models from MedSegBench are available and their names can be found in [dataset.py](src/dataset.py), but their respective weights still need to be downloaded from zenodo.
+
+
+```python
+DATASETS = {
+        'CovidQUExMSBench',
+        'AbdomenUSMSBench',
+        'NucleiMSBench',
+        'BusiMSBench',
+        'DynamicNuclearMSBench'
+}
+
+MODELS = [
+        'UNet',
+        'AttentionUNet',
+        'ResUNet',
+        'ResAttentionUNet',
+        'DenseUNet',
+        'DenseAttentionUNet',
+        'UNetPP'
+]
+```
+
+#### Training & Testing
+
+**Training:**
+```bash
+cd src/
+python train.py 
+```
+
+**Testing:**
+```bash
+cd src/ 
+python test.py 
+```
+
+## Datasets
+
+## Models
+
+## Results  
+
+
+## References
+
+
--- src/dataset.py
+++ src/dataset.py
@@ -6,67 +6,8 @@ import medsegbench
 from PIL import Image
 import config
 
-
-#
-# def parseNpData(data, imgNumDim: int, labelNumDim: int):
-#     """Return (trainImages, trainLabels, valImages, valLabels, testImages, testLabels)"""
-#     # np dim format is <H, W, C>, tensor is <C, H, W>
-#     # If the dim is 4, reorder to match tensor format, if dim is 3, add extra dim for channel
-#     trainImages = data["train_images"].transpose(0, 3, 1, 2) if imgNumDim == 4 else np.expand_dims(data["train_images"], axis=1)
-#     valImages = data["val_images"].transpose(0, 3, 1, 2) if imgNumDim == 4 else np.expand_dims(data["val_images"], axis=1)
-#     testImages = data["test_images"].transpose(0, 3, 1, 2) if imgNumDim == 4 else np.expand_dims(data["test_images"], axis=1)
-#
-#     trainLabels = data["train_label"].transpose(0, 3, 1, 2) if labelNumDim == 4 else np.expand_dims(data["train_label"], axis=1)
-#     valLabels = data["val_label"].transpose(0, 3, 1, 2) if labelNumDim == 4 else np.expand_dims(data["val_label"], axis=1)
-#     testLabels = data["test_label"].transpose(0, 3, 1, 2) if labelNumDim == 4 else np.expand_dims(data["test_label"], axis=1)
-#
-#     return trainImages, trainLabels, valImages, valLabels, testImages, testLabels
-#
-# def getDataset(dataFilePath: str, transform = None):
-#     """Return (trainImageDataset, valImageDataset, testImageDataset)"""
-#     data = np.load(dataFilePath)
-#     trainImages, trainLabels, valImages, valLabels, testImages, testLabels = parseNpData(data, data["train_images"].ndim, data["train_label"].ndim)
-#     return (ModelDataset(trainImages, trainLabels, transform),
-#             ModelDataset(valImages, valLabels, transform),
-#             ModelDataset(testImages, testLabels, transform))
-#
-#
-# class ModelDataset(Dataset):
-#     def __init__(self, images, labels, transform = None):
-#         self.images = images
-#         self.labels = labels
-#
-#         self.transform = transform
-#
-#     def __len__(self):
-#         return len(self.images)
-#
-#     def __getitem__(self, index):
-#         image = self.images[index].astype(np.float32)
-#         label = self.labels[index].astype(np.float32)
-#
-#         image = torch.from_numpy(image) / 255.0
-#         label = torch.from_numpy(label)
-#
-#         # Note: transform is not used on label
-#         if self.transform:
-#             return self.transform(image), label
-#         else:
-#             return image, label
-#
-# # train, val, test = getDataset(const.COVID_FILE_PATH)
-#
-# # img, mask = train[1]
-#
-# # print(f"Image shape: {img.shape}")  # should be (1, 256, 256)
-# # print(f"Mask shape: {mask.shape}")  # should be (1, 256, 256)
-# # print(f"Image min/max: {img.min().item()}/{img.max().item()}")
-# # print(f"Mask min/max: {mask.min().item()}/{mask.max().item()}")
-
 # https://docs.pytorch.org/vision/0.8/transforms.html
 # https://docs.pytorch.org/vision/stable/transforms.html
-
-
 
 class MedSegBenchDataset(Dataset):
     def __init__(self, dataset_instance, split='train'):
